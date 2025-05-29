82374603539d25d908f47e15613840393a1425fd
Rakesh Raghavaraju <rakesh.srirajaraghavaraju@queensu.ca>
2025-05-29 11:21:59 -0400

    Delete CourseMaterial/CPP directory
    Deleted the CPP files

--- CourseMaterial/CPP/CPP_Complex/main.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "ComplexDT.h"
-
-int main(){
-	ComplexDT objComplex01(2,3);
-	//std::cout<<"The complex number is:"<<objComplex01.getReal()<<"+"<<objComplex01.imag<<"i"<<std::endl;
-	std::cout<<"The complex number is: "<<objComplex01.getReal()<<"+"<<objComplex01.getImag()<<"i"<<std::endl;
-	return 0;
-}
