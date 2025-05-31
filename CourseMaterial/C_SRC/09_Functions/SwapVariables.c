5165e7450a8de175054adf386a89625b59699fda
Rakesh Raghavaraju <rakesh.srirajaraghavaraju@queensu.ca>
2025-05-30 23:55:11 -0400

    Create SwapVariables.c
    Add a swap function for pass by reference and value

---
+++ CourseMaterial/C_SRC/09_Functions/SwapVariables.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void swap_byvalue(int a, int b){
+  // This doesn't work
+  int tmp;
+  tmp = a;
+  a = b; 
+  b=tmp;
+}
+
+void swap_byref(int* ptr_a, int* ptr_b){
+  int tmp;
+  tmp = *ptr_a;
+  *ptr_a = *ptr_b;
+  *ptr_b = tmp;
+}
+
+int main(){
+{
+  int a = 2, b = 100;
+
+  printf(" The intial value of a is %i and b is %i", a , b);
+  swap_byvalue(a,b);
+  printf(" After calling swap_byvalue value of a is %i and b is %i", a , b);
+
+  swap_byref(&a,&b);
+  printf(" After calling swap_byref value of a is %i and b is %i", a , b);
+  return 0;
+}
