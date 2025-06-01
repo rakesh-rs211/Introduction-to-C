8b976843ae7b7ae46d7365a802bee46204576592
Rakesh Raghavaraju <rakesh.srirajaraghavaraju@queensu.ca>
2025-05-31 22:05:56 -0400

    Update Datastructures.h

--- CourseMaterial/C_SRC/10_Structures/DataStructures/Datastructures.h
+++ CourseMaterial/C_SRC/10_Structures/DataStructures/Datastructures.h
@@ -1,1 +1,17 @@
+#ifndef DataStructures_COSS_H
+#define DataStructures_COSS_H
 
+/* Singly-connected node */
+struct Node_LL{
+  int data;
+  struct Node_LL *next;
+};
+
+/*  Binary Tree Struct */
+struct Node_BT{
+  int data;
+  struct Node_BT *left;
+  struct Node_BT *left;
+};
+
+#endif
