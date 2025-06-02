e0263444500542b3b45490e63052de8e162aebbc
Rakesh Raghavaraju <rakesh.srirajaraghavaraju@queensu.ca>
2025-06-01 20:47:29 -0400

    Update CreateMesh.c
    Moving printMesh to Mesh.h

--- CourseMaterial/C_SRC/10_Structures/Mesh/CreateMesh.c
+++ CourseMaterial/C_SRC/10_Structures/Mesh/CreateMesh.c
@@ -35,31 +35,6 @@ void createMesh(Mesh* meshptr){
         return ;
 }
 
-void printMesh(Mesh mesh){
-        FILE *fptr;
-        Point2D *pts;
-        Element *elems;
-        pts=mesh.pts; elems=mesh.eltopo;
-        fptr = fopen("Beam.msh","w");
-        /* Write header to the gmsh file */
-        fprintf(fptr,"$MeshFormat\n2.2 0 8\n$EndMeshFormat\n");
-
-        /* Write the Nodal Coordinates */
-        fprintf(fptr,"$Nodes\n%d\n",mesh.NNodes);
-        for(int i=0; i<mesh.NNodes; i++){
-                fprintf(fptr,"%d %f %f 0.0\n",i+1,pts[i].x,pts[i].y);
-        }
-        fprintf(fptr,"$EndNodes\n");
-        /* Write the Element Topology */
-        fprintf(fptr,"Elements\n%d\n",mesh.NElems);
-        for(int i=0; i<mesh.NElems; i++){
-                fprintf(fptr,"%d 3 0 %d %d %d %d\n",i+1,elems[i].NodeID[0]+1,elems[i].NodeID[1]+1,\
-                                                        elems[i].NodeID[2]+1,elems[i].NodeID[3]+1);
-        }
-        fprintf(fptr,"$EndElements\n");
-        fclose(fptr);
-        return;
-}
 
 int main(int argc, char argv[]){
         Mesh mesh;
