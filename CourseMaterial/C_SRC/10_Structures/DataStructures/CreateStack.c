ed11cefb0414e10e6bd5a701f8046d78dcaa577a
Rakesh Raghavaraju <rakesh.srirajaraghavaraju@queensu.ca>
2025-06-05 00:57:03 -0400

    Update CreateStack.c
    Fixed pointer bugs

--- CourseMaterial/C_SRC/10_Structures/DataStructures/CreateStack.c
+++ CourseMaterial/C_SRC/10_Structures/DataStructures/CreateStack.c
@@ -5,14 +5,19 @@
 int main(int argc, char **argv){
   int arr[5]={100, 324, 432, 36, 1};
   int i,n=5;
-  struct NodeLL* head;
+  struct Stack stk;
+  struct NodeLL* nptr;
   /* Create a stack from the array, arr */
+  stk.head = NULL; nptr = NULL;
   for(i=0; i<n;i++){
-    stack_push(head,arr[i]);
+    stack_push(&stk,arr[i]);
+    printf("%p \n",stk.head);
   }
-  /* Pop and Print the stack */
-  while(isEmptyLL(head)){
-    printf("%d \n",stack_pop(head));
+  /* Print the stack */
+  nptr = stk.head;
+  while(nptr != NULL){
+    printf("%d \n",nptr->data);
+    nptr = nptr->next;
   }
   return 0;
 }
